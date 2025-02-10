2c1ee2cf3fac1d54ad5bf120483d12faf69e9486
nadhshafres <nadhshafres@gmail.com>
2025-02-10 16:14:23 +0700

    implement custom app bar

--- lib/features/report/pages/report_page.dart
+++ lib/features/report/pages/report_page.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:nutri_call_app/helpers/widget/custom_app_bar.dart';
 
 class ReportPage extends HookConsumerWidget {
   const ReportPage({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return const Scaffold(
-      body: Center(
-        child: Text('Report Page'),
+      appBar: CustomAppBar(
+        title: 'Report',
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        child: Center(
+          child: Text('Report Page'),
+        ),
       ),
     );
   }
