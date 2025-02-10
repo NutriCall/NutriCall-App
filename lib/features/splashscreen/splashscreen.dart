93dfda0817b74d5211bba88d9dd5cdb033548147
nadhshafres <nadhshafres@gmail.com>
2025-02-10 12:28:33 +0700

    implement splashscreen

--- lib/features/splashscreen/splashscreen.dart
+++ lib/features/splashscreen/splashscreen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:nutri_call_app/extensions/build_context.ext.dart';
 import 'package:nutri_call_app/routers/router_name.dart';
+import 'package:nutri_call_app/utils/app_color.dart';
 import 'package:nutri_call_app/utils/assets.gen.dart';
 
 class Splashscreen extends StatefulHookConsumerWidget {
@@ -13,9 +13,17 @@ class Splashscreen extends StatefulHookConsumerWidget {
 }
 
 class _SplashscreenState extends ConsumerState<Splashscreen> {
+  double _scale = 0.2; 
+
   @override
   void initState() {
     super.initState();
+    Future.delayed(const Duration(milliseconds: 500), () {
+      setState(() {
+        _scale = 1.0; 
+      });
+    });
+
     Future.delayed(const Duration(seconds: 3), () {
       context.push(RouteName.main);
     });
@@ -24,31 +32,22 @@ class _SplashscreenState extends ConsumerState<Splashscreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFF0F1323),
-      body: Column(
-        children: [
-          const Spacer(),
-          Stack(
-            children: [
-              Image.asset(
-                Assets.images.mountainElement.path,
-                fit: BoxFit.cover,
-                height: context.deviceHeight * 0.6,
-                width: context.deviceWidth,
-              ),
-              Center(
-                child: Hero(
-                  tag: 'logo',
-                  child: Image.asset(
-                    Assets.images.logoApp.path,
-                    height: 94,
-                  ),
-                ),
-              ),
-            ],
+      backgroundColor: AppColor.disabledGreen,
+      body: Center(
+        child: AnimatedScale(
+          scale: _scale,
+          duration: const Duration(seconds: 1),
+          curve: Curves.easeOutBack, 
+          child: Hero(
+            tag: 'logo',
+            child: Image.asset(
+              Assets.images.logoApp.path,
+              height: 180,
+            ),
           ),
-        ],
+        ),
       ),
     );
   }
 }
+
