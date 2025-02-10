fe53e6ea4ddfd8a5bf41e4bdba82b691e393bd32
nadhshafres <nadhshafres@gmail.com>
2025-02-10 23:45:13 +0700

    adjust profile page

--- lib/features/profile/widget/profile_detail_item_widget.dart
+++ lib/features/profile/widget/profile_detail_item_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:nutri_call_app/utils/app_color.dart';
 
@@ -7,51 +8,52 @@ class ProfileDetailItem extends StatelessWidget {
   final String value;
 
   const ProfileDetailItem({
-    Key? key,
+    super.key,
     required this.label,
     required this.value,
-  }) : super(key: key);
+  });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 6.0, horizontal: 73.0),
+      padding: const EdgeInsets.symmetric(horizontal: 80, vertical: 4),
       child: Row(
-        children: [
-          Expanded(
-            flex: 2,
-            child: Text(
-              label,
+          mainAxisSize: MainAxisSize.min, 
+          children: [
+            Expanded(
+              flex: 2,
+              child: Text(
+                label,
+                style: GoogleFonts.poppins(
+                  fontSize: 14,
+                  color: AppColor.grey,
+                ),
+                textAlign: TextAlign.left,
+              ),
+            ),
+            const Gap(10),
+            Text(
+              ':',
               style: GoogleFonts.poppins(
                 fontSize: 14,
                 color: AppColor.grey,
               ),
-              textAlign: TextAlign.left,
             ),
-          ),
-          const SizedBox(width: 10),
-          Text(
-            ':',
-            style: GoogleFonts.poppins(
-              fontSize: 14,
-              color: AppColor.grey,
-            ),
-          ),
-          const SizedBox(width: 10),
-          Expanded(
-            flex: 4,
-            child: Text(
-              value,
-              style: GoogleFonts.poppins(
-                fontSize: 14,
-                fontWeight: FontWeight.w500,
-                color: AppColor.darkGrey,
+            const Gap(10),
+            Expanded(
+              flex: 4,
+              child: Text(
+                value,
+                style: GoogleFonts.poppins(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                  color: AppColor.darkGrey,
+                ),
+                textAlign: TextAlign.left,
               ),
-              textAlign: TextAlign.left,
             ),
-          ),
-        ],
-      ),
+          ],
+        ),
     );
   }
 }
