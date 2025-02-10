3d6b3283e40f50af2cc86477574552dd7af965c2
nadhshafres <nadhshafres@gmail.com>
2025-02-10 16:15:21 +0700

    add recipe page

--- lib/routers/router_name.dart
+++ lib/routers/router_name.dart
@@ -2,4 +2,5 @@ class RouteName {
   static const String main = '/';
   static const String splashscreen = '/splashscreen';
   static const String login = '/login';
+  static const String recipePage = '/recipe-page';
 }
